4cc54452fa863c294439366fda150533888d88d6
Filip Dobrovolný <brnopcman@gmail.com>
2016-05-04 00:56:55 +0200

    ADDED: VanilaMinecraftServer .stop(), ._get_jar_file(), ._get_java_options()

--- GSMT/servers_library/minecraft.py
+++ GSMT/servers_library/minecraft.py
@@ -1,9 +1,59 @@
 """This module contains gameservers for minecraft."""
+import os
+
 from GSMT.game_server import Server
 
 
 class VanilaMinecraftServer(Server):
-    """Server class for vanila Minecraft server"""
+    """Server class for vanila Minecraft server.
+
+    - example config file ::
+
+        [main]
+        # server with name "main"
+        type = minecraft_vanila
+        # in direcory /opt/mc_server
+        path = /opt/mc_server
+        # start server on daemon start
+        start_on_daemon_start = True
+        # with java <options>
+        java_options = -Xms1024M -Xmx2048M
+        # name of Jar file
+        jar_file = minecraft_server.1.9.2.jar
+
+    """
+
     def start(self):
-        self._start(["java", "-Xms1024M", "-Xmx2048M", "-jar",
-                     self.path + "minecraft_server.1.9.2.jar", "nogui"])
+        """Start java minecraft server."""
+        command = ["java"]
+
+        # Add java options
+        command.extend(self._get_java_options())
+
+        # jar file
+        command.extend(["-jar", os.path.join(self.path, self._get_jar_file()),
+                        "nogui"])
+        self._start(command)
+
+    def stop(self):
+        """Send stop command to server."""
+        self.write("stop")
+
+    def _get_jar_file(self):
+        """Return the name of jar_file.
+
+        Default: 'minecraft_server.jar'
+
+        :rtype: str
+        """
+        return self.config.get("jar_file", "minecraft_server.jar")
+
+    def _get_java_options(self):
+        """Return java_options.
+
+        Default: '-Xms1024M -Xmx2048M'
+
+        :rtype: list
+        """
+        return (self.config.get("java_options", "-Xms1024M -Xmx2048M")
+                    .split(" "))
